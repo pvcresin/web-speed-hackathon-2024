c192b10d943b8794fb1162f22e3c0f71e88c5fbc
pvcresin <pvcresin0730@gmail.com>
2024-03-23 20:28:00 +0900

    Use jpg instead of jxl in client

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,7 +1,5 @@
 /// <reference types="@types/serviceworker" />
 
-import { transformJpegXLToBmp } from './transformJpegXLToBmp';
-
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
 });
@@ -9,17 +7,3 @@ self.addEventListener('install', (ev: ExtendableEvent) => {
 self.addEventListener('activate', (ev: ExtendableEvent) => {
   ev.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', (ev: FetchEvent) => {
-  ev.respondWith(onFetch(ev.request));
-});
-
-async function onFetch(request: Request): Promise<Response> {
-  const res = await fetch(request);
-
-  if (res.headers.get('Content-Type') === 'image/jxl') {
-    return transformJpegXLToBmp(res);
-  } else {
-    return res;
-  }
-}
