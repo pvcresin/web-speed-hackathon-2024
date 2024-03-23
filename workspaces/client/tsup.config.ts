375a8efa137a1114a538c1c325f6a11c9bb1a155
pvcresin <pvcresin0730@gmail.com>
2024-03-23 18:10:23 +0900

    Do not treeshake for sw failed

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -61,7 +61,7 @@ export default defineConfig(async (): Promise<Options[]> => {
       sourcemap: true,
       splitting: true,
       target: ['chrome123'],
-      treeshake: true,
+      treeshake: false,
     },
   ];
 });
