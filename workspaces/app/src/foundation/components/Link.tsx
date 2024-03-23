a0384571509b6d05a91cacdbd24ee1fa2b58519f
pvcresin <pvcresin0730@gmail.com>
2024-03-23 14:45:58 +0900

    Enable router Link

--- workspaces/app/src/foundation/components/Link.tsx
+++ workspaces/app/src/foundation/components/Link.tsx
@@ -1,12 +1,18 @@
+import { Link as _Link } from 'react-router-dom';
+
 type Props = {
   children: React.ReactNode;
   to?: string;
 } & JSX.IntrinsicElements['a'];
 
 export const Link: React.FC<Props> = ({ children, to, ...rest }) => {
-  return (
-    <a href={to} {...rest}>
-      {children}
-    </a>
-  );
+  if (to) {
+    return (
+      <_Link to={to} {...rest} ref={null}>
+        {children}
+      </_Link>
+    );
+  }
+
+  return <a {...rest}>{children}</a>;
 };
