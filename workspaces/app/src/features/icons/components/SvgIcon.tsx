3cbbcd8d5dca6eaa176df8c9c4c7473589ba199f
pvcresin <pvcresin0730@gmail.com>
2024-03-23 22:20:32 +0900

    Import individual icon

--- workspaces/app/src/features/icons/components/SvgIcon.tsx
+++ workspaces/app/src/features/icons/components/SvgIcon.tsx
@@ -1,14 +1,32 @@
-import * as Icons from '@mui/icons-material';
+import { ArrowBack, Close, Favorite, FavoriteBorder, NavigateNext, Search } from '@mui/icons-material';
+import { useMemo } from 'react';
 
 type Props = {
   color: string;
   height: number;
-  type: keyof typeof Icons;
+  type: 'ArrowBack' | 'Close' | 'Favorite' | 'FavoriteBorder' | 'NavigateNext' | 'Search';
   width: number;
 };
 
 export const SvgIcon: React.FC<Props> = ({ color, height, type, width }) => {
-  // eslint-disable-next-line
-  const Icon = Icons[type];
+  const Icon = useMemo(() => {
+    switch (type) {
+      case 'ArrowBack':
+        return ArrowBack;
+      case 'Close':
+        return Close;
+      case 'Favorite':
+        return Favorite;
+      case 'FavoriteBorder':
+        return FavoriteBorder;
+      case 'NavigateNext':
+        return NavigateNext;
+      case 'Search':
+        return Search;
+      default:
+        throw Error(`Unknown icon type: ${type}`);
+    }
+  }, [type]);
+
   return <Icon style={{ color, height, width }} />;
 };
