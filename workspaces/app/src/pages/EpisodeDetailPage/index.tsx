99f821facfd5e69209ef9ab8e4f6dbfea17ebf8b
pvcresin <pvcresin0730@gmail.com>
2024-03-24 12:54:34 +0900

    Reduce layout shift on episode detail page

--- workspaces/app/src/pages/EpisodeDetailPage/index.tsx
+++ workspaces/app/src/pages/EpisodeDetailPage/index.tsx
@@ -5,37 +5,44 @@ import invariant from 'tiny-invariant';
 
 import { useBook } from '../../features/book/hooks/useBook';
 import { EpisodeListItem } from '../../features/episode/components/EpisodeListItem';
-import { useEpisode } from '../../features/episode/hooks/useEpisode';
 import { Box } from '../../foundation/components/Box';
 import { Flex } from '../../foundation/components/Flex';
 import { Separator } from '../../foundation/components/Separator';
+import { Spacer } from '../../foundation/components/Spacer';
 import { Space } from '../../foundation/styles/variables';
 
 import { ComicViewer } from './internal/ComicViewer';
+
+const EpisodeList: React.FC<{ bookId: string }> = ({ bookId }) => {
+  const { data: book } = useBook({ params: { bookId } });
+
+  return (
+    <Box aria-label="エピソード一覧" as="section" px={Space * 2}>
+      <Flex align="center" as="ul" direction="column" justify="center">
+        {book.episodes.map((episode) => (
+          <EpisodeListItem key={episode.id} bookId={bookId} episodeId={episode.id} />
+        ))}
+      </Flex>
+    </Box>
+  );
+};
 
 const EpisodeDetailPage: React.FC = () => {
   const { bookId, episodeId } = useParams<RouteParams<'/books/:bookId/episodes/:episodeId'>>();
   invariant(bookId);
   invariant(episodeId);
 
-  const { data: book } = useBook({ params: { bookId } });
-  const { data: episode } = useEpisode({ params: { episodeId } });
-
   return (
     <Box>
       <section aria-label="漫画ビューアー">
-        <ComicViewer episodeId={episode.id} />
+        <ComicViewer episodeId={episodeId} />
       </section>
 
       <Separator />
 
-      <Box aria-label="エピソード一覧" as="section" px={Space * 2}>
-        <Flex align="center" as="ul" direction="column" justify="center">
-          {book.episodes.map((episode) => (
-            <EpisodeListItem key={episode.id} bookId={bookId} episodeId={episode.id} />
-          ))}
-        </Flex>
-      </Box>
+      <Suspense fallback={<Spacer height={121 * 15} />}>
+        <EpisodeList bookId={bookId} />
+      </Suspense>
     </Box>
   );
 };
