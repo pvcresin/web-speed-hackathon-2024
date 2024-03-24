e1b4c0582b025b7e7177457f9b67623a2f6de6f2
pvcresin <pvcresin0730@gmail.com>
2024-03-24 13:50:44 +0900

    Cache static assets

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -2,6 +2,13 @@ import { createMiddleware } from 'hono/factory';
 
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
   await next();
-  c.res.headers.append('Cache-Control', 'private');
-  c.res.headers.append('Cache-Control', 'no-store');
+
+  const { path } = c.req;
+  if (path.startsWith('/assets') || path.startsWith('/images') || path.startsWith('/static')) {
+    c.res.headers.append('Cache-Control', 'public, max-age=31536000, immutable');
+    return;
+  } else {
+    c.res.headers.append('Cache-Control', 'private');
+    c.res.headers.append('Cache-Control', 'no-store');
+  }
 });
